e7fc6b68779204d2be60279e634367a4999feecd
Antonio Segovia <antonio.segovia@iris.net>
2020-06-05 15:24:20 +0100

    Updates

---
+++ precache-manifest.43d715fe49341ab8e7a3d4123549c3d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb38ad765f8a30931ae1bb415c6f0bf0",
+    "url": "/tet/index.html"
+  },
+  {
+    "revision": "0e509734d6725705ee6b",
+    "url": "/tet/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4dae2d27d6a913bbb33d",
+    "url": "/tet/static/js/2.af903c5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tet/static/js/2.af903c5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e509734d6725705ee6b",
+    "url": "/tet/static/js/main.433716ca.chunk.js"
+  },
+  {
+    "revision": "f1b092463868c62ce6d8",
+    "url": "/tet/static/js/runtime-main.a5bf8f4d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tet/static/media/logo.5d5d9eef.svg"
+  }
+]);
